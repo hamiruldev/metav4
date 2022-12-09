471eca256f47726faff06a96a7417fc8e270b76c
SU1_Basiir <softwareu1@i-smart.com.sg>
2022-12-09 17:49:38 +0800

    tmbh www

--- src/page/Nomenu.jsx
+++ src/page/Nomenu.jsx
@@ -20,7 +20,7 @@ const Nomenu = ({ url }) => {
         }}
         onClick={() => {
           // navigate(`../`);
-          window.location.assign("https://i-smart.com.sg/")
+          window.location.assign("https://www.i-smart.com.sg/")
           // window.location.assign("https://2vr360.com/basiir/test32/")
 
           
