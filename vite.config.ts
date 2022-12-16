13ae0736c51b47be911da9fd6ea6680bbded9031
SU1_Basiir <softwareu1@i-smart.com.sg>
2022-12-16 16:04:36 +0800

    dah rosak

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 //vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // mode: "development",
+  // base: "/portfolio/", // mode: "development",
+  // base: "/services/", // mode: "development",
+  base: "/", // mode: "development",
   // base: "/basiir/test35/", // mode: "staging",
 });
